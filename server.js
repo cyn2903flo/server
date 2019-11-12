e0dfc5862590d7bc284dfcc656dca9a32494b70c
cyn2903 <cyn2903flo@gmail.com>
2019-11-12 11:59:30 -0600

    se instalo mongoose y cors

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-//listo
+const express  = require('express');
+
+const app = express();
+
+const PORT = 4532;
+
+
+app.get('/',(request, response)=>{
+    response.send({message:'Pingüino'})
+});
+
+app.get('/home',(request,response)=>{
+    response.send({message:'Desde home'});
+});
+
+app.get('/user/z',(req,res)=>{
+    const {parametro} = req.params
+
+    res.send({message:`El usuario con el id:${parametro} fue buscado`})
+});
+
+
+app.get('/gato/search',(req,res)=>{
+    const queries = req.query
+    res.send({queries:queries})
+});
+
+app.listen(PORT,()=>{
+    console.log(`Se ha inicializado el servidor en el puerto ${PORT}`)
+});
